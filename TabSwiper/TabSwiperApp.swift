b015659ba6c887e89650f1e62c45b5c388e1adce
Matthew Justin <mtwjstn@gmail.com>
2023-03-27 17:53:57 +0100

    Initial Commit

---
+++ TabSwiper/TabSwiperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabSwiperApp.swift
+//  TabSwiper
+//
+//  Created by MattJust on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TabSwiperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
